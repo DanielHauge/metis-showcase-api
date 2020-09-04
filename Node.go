dd6b11b0957348fafe1fdc92fa8d62e42cb5161b
Daniel <animcuil@gmail.com>
2020-09-04 09:47:06 +0200

    Added test navigation data structure

--- Node.go
+++ Node.go
@@ -1,1 +1,12 @@
 package main
+
+type NodeData struct{
+	Type int32 `json:"type"`
+	Url string `json:"url"`
+	Title string `json:"title"`
+}
+
+type Node struct{
+	Data NodeData `json:"data"`
+	Children map[string]Node `json:"children"`
+}
